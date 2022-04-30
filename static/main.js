962c1ccb63d84dc03de7f38dcdec2fe372963d49
nichlock <nichlock@gmail.com>
2022-04-30 17:10:43 -0400

    Working commit for multiple types; tabulated data
    Modified frontend to take data from independent sheets (for different printer types)

--- static/main.js
+++ static/main.js
@@ -1,25 +1,41 @@
+function createTableRow(ticket, que_position) {
+  if (ticket === undefined || ticket === null) {
+    // Return a title row
+    return $('<tr>').addClass('ticket-list-header')
+      .append($('<th>').text('Ticket'))
+      .append($('<th>').text('Type'))
+      .append($('<th>').text('Status'))
+      .append($('<th>').text('Que Position'))
+      .append($('<th>').text('Total Cost'));
+  }
+  let row = $('<tr>').addClass('ticket-list-row')
+    .append($('<td>').addClass('ticket-id').text(ticket.uuid))
+    .append($('<td>').addClass('ticket-type').text('Ultimaker (todo)'))
+    .append($('<td>').addClass('ticket-status').text(ticket.status))
+    .append($('<td>').addClass('ticket-que'))
+    .append($('<td>').addClass('ticket-cost').text(ticket.cost));
+
+  if (ticket.cost[0] !== '$')
+    row.children('.ticket-cost').addClass('empty');
+
+  if (TicketUtils.isStatusWaiting(ticket.status))
+    row.children('.ticket-que').append($('<span>').text(que_position)).append($('<span>').text(` / ${ultimaker_tickets.getTotalWaitingTickets() + 1}`));
+
+  return row;
+}
+
+
 function updateTicketDomList(ticket_indexes) {
-  container = $('#ticket-info-box').empty();
+  container = $('#ticket-info-table').empty();
+  container.append(createTableRow());
 
   found_tickets = ticket_indexes['found'];
   unknown_tickets = ticket_indexes['not_found'];
 
-  getAllTickets().then((all_tickets) => {
+  ultimaker_tickets.getAllTickets().then((all_tickets) => {
     for (let t = 0; t < found_tickets.length; t++) {
-      ticket = all_tickets[found_tickets[t]];
-      list_item = $('<li>')
-        .append($('<span>').text(`${ticket.uuid}: `))
-
-      list_item.append($('<span>').text(`Status: ${ticket.status}`));
-
-      if (statusIsWaiting(ticket.status)) {
-        // list_item.append(`, `);
-        list_item.append($('<span>').text(` (behind ${found_tickets[t] + 1} of ${getTotalWaitingTickets() + 1} prints.)`));
-      }
-
-      container.append(list_item);
+      container.append(createTableRow(all_tickets[found_tickets[t]], found_tickets[t] + 1));
     }
-    console.log(unknown_tickets);
     for (let t = 0; t < unknown_tickets.length; t++) {
       list_item = $('<li>')
         .append($('<span>').text(`${unknown_tickets[t]} not found. It may have not been registered in our system yet.`))
@@ -32,11 +48,20 @@ function getNewTickets(evt) {
   let ticket_request = evt.target.value;
   ticket_request = ticket_request.split(/[\s,]+/);
   ticket_request = ticket_request.filter((str) => { return str.length > 0 });
-  findTickets(ticket_request).then(ticket_indexes => updateTicketDomList(ticket_indexes));
+  ultimaker_tickets.findTickets(ticket_request).then(ticket_indexes => updateTicketDomList(ticket_indexes));
 }
 
 function initialize() {
   $('#ticket-id-box').change((evt) => getNewTickets(evt));
+
+
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+
+  if (params.t !== null) {
+    $('#ticket-id-box').val(params.t).change();
+  }
 }
 
 document.onreadystatechange = function () {
--- static/tickets.js
+++ static/tickets.js
@@ -1,116 +1,128 @@
-const SHEET_ID = '1k0ZiJ1El2J1gxfqp6boEx7E2OTiSW87pCp_c9j0jv0Q'
-const API_KEY = 'AIzaSyDHaowMtvRwFaYBH--r4utAAd6XJ5bc-6c';
-const SHEET_RANGE = 'A1:C1000';
-const GAPI_QUERY = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${SHEET_RANGE}?key=${API_KEY}`
+const _SHEET_ID = '1k0ZiJ1El2J1gxfqp6boEx7E2OTiSW87pCp_c9j0jv0Q'
+const _API_KEY = 'AIzaSyDHaowMtvRwFaYBH--r4utAAd6XJ5bc-6c';
+const _SHEET_RANGE = "A1:D1000";
 
-const COL_MAP = { 'UUID': 0, 'PRIORITY': 1, 'STATUS': 2 }
+const _COL_MAP = { 'UUID': 0, 'PRIORITY': 1, 'STATUS': 2, 'COST': 3 }
 
-let __global_tickets = null;
-let __global_tickets_lock = false;
-let __number_of_waiting_tickets = 0;
-
-function statusIsWaiting(status) {
-  return status.toLowerCase().includes('waiting to print')
+class TicketUtils {
+  static isStatusWaiting(status) {
+    return status.toLowerCase().includes('waiting to print')
+  }
 }
 
-function getTotalWaitingTickets() {
-  return __number_of_waiting_tickets;
-}
+class TicketList {
+  constructor(sheet_name) {
+    this.GAPI_QUERY = `https://sheets.googleapis.com/v4/spreadsheets/${_SHEET_ID}/values/'${sheet_name}'!${_SHEET_RANGE}?key=${_API_KEY}`;
+    this.__global_tickets = null;
+    this.__global_tickets_lock = false;
+    this.__number_of_waiting_tickets = 0;
+    this.getSpreadsheetValues() // Perform inital page load
+  }
+  /**
+   * @returns The total number of tickets that are waiting in the que to be printed.
+   */
+  getTotalWaitingTickets() {
+    return this.__number_of_waiting_tickets;
+  }
 
-/**
- * Filter the recived data and bring it into a more usable formats
- */
-function handleNewData(data) {
-  __global_tickets_lock = true;
-  __global_tickets = [];
-  __number_of_waiting_tickets = 0;
-  // Convert to dict for readability and remove empty/invalid rows
-  for (let r = 0; r < data.values.length; r++) {
-    let row = data.values[r];
-    if (row.length !== Object.keys(COL_MAP).length) {
-      continue;
-    }
-    __global_tickets.push({
-      'uuid': row[COL_MAP['UUID']],
-      'priority': Number(row[COL_MAP['PRIORITY']]),
-      'status': row[COL_MAP['STATUS']]
-    });
-
-    if (statusIsWaiting(row[COL_MAP['STATUS']]))
-      __number_of_waiting_tickets++;
-  };
-
-  __global_tickets.sort((a, b) => { return b.priority - a.priority });
-  __global_tickets_lock = false;
-}
-
-/**
- * Get and typecast data from the public-facing spreadsheet.
- * 
- * It is sadly not possible to query based on data (i.e. search), so instead we
- * take all available data and filter on the client.
- * 
- */
-function getSpreadsheetValues() {
-  return $.get({
-    url: GAPI_QUERY,
-    type: 'GET',
-    dataType: 'json',
-  }).done((data) => { handleNewData(data); })
-    .fail((xhr) => {
-      let err = `${xhr.statusText} ${xhr.status}`;
-      try {
-        let response = JSON.parse(xhr.responseText);
-        err += `\n${response.error.status} (${response.error.message})`
-      } catch { }
-      console.log(err);
-      alert(err);
-    });
-}
-
-function getAllTickets(force_refresh = false) {
-  return new Promise(function (resolve, reject) {
-    if (__global_tickets !== null && !force_refresh) {
-      while (__global_tickets_lock === true) { }
-      resolve(__global_tickets);
-    }
-    else {
-      getSpreadsheetValues()
-        .then(() => resolve(__global_tickets))
-        .catch(error => reject(error));
-    }
-  });
-}
-
-/**
- * Find the list of tickets by ID.
- * 
- * @param {} search_list An array of string IDs to search.
- * @returns An ordered array of IDs matching the tickets in __global_tickets.
- */
-async function findTickets(search_list) {
-  if (!Array.isArray(search_list)) search_list = [search_list];
-
-  return new Promise(function (resolve, reject) {
-    getAllTickets().then((all_tickets) => {
-      unfound_tickets = search_list;
-      resulting_ids = [];
-
-      for (let i = 0; i < all_tickets.length; i++) {
-        let idx = unfound_tickets.findIndex((el) => { return (el === all_tickets[i]['uuid']) })
-        if (idx >= 0) {
-          resulting_ids.push(i);
-          unfound_tickets.splice(idx, 1);
-        }
+  /**
+   * Filters the recived data and bring it into a more usable formats
+   */
+  handleNewData(data) {
+    this.__global_tickets_lock = true;
+    this.__global_tickets = [];
+    this.__number_of_waiting_tickets = 0;
+    // Convert to dict for readability and remove empty/invalid rows
+    for (let r = 0; r < data.values.length; r++) {
+      let row = data.values[r];
+      if (row.length !== Object.keys(_COL_MAP).length) {
+        continue;
       }
+      this.__global_tickets.push({
+        'uuid': row[_COL_MAP['UUID']],
+        'priority': Number(row[_COL_MAP['PRIORITY']]),
+        'status': row[_COL_MAP['STATUS']],
+        'cost': row[_COL_MAP['COST']]
+      });
 
-      resolve({'found': resulting_ids, 'not_found': unfound_tickets});
-    }).catch((error) => reject(error));
-  });
+      if (TicketUtils.isStatusWaiting(row[_COL_MAP['STATUS']]))
+        this.__number_of_waiting_tickets++;
+    };
+
+    this.__global_tickets.sort((a, b) => { return b.priority - a.priority });
+    this.__global_tickets_lock = false;
+  }
+
+  /**
+   * Get and typecast data from the public-facing spreadsheet.
+   * 
+   * It is sadly not possible to query based on data (i.e. search), so instead we
+   * take all available data and filter on the client.
+   * 
+   */
+  getSpreadsheetValues() {
+    return $.get({
+      url: this.GAPI_QUERY,
+      type: 'GET',
+      dataType: 'json',
+    }).done((data) => { this.handleNewData(data); })
+      .fail((xhr) => {
+        let err = `${xhr.statusText} ${xhr.status}`;
+        try {
+          let response = JSON.parse(xhr.responseText);
+          err += `\n${response.error.status} (${response.error.message})`
+        } catch { }
+        console.log(err);
+        alert(err);
+      });
+  }
+  /**
+   * Gets a shallow copy of the full list of tickets.
+   * 
+   * If tickets have not yet been loaded, this will load them.
+   * 
+   * @returns A promise containing the list of tickets when resolved.
+   */
+  getAllTickets(force_refresh = false) {
+    return new Promise((resolve, reject) => {
+      if (this.__global_tickets !== null && !force_refresh) {
+        while (this.__global_tickets_lock === true) { }
+        resolve(this.__global_tickets);
+      }
+      else {
+        this.getSpreadsheetValues()
+          .then(() => resolve(this.__global_tickets))
+          .catch(error => reject(error));
+      }
+    });
+  }
+
+  /**
+   * Get a list of tickets by IDs.
+   * 
+   * @param {Array} search_list An array of string IDs to search.
+   * @returns An ordered array of IDs matching the tickets in __global_tickets.
+   */
+  async findTickets(search_list) {
+    if (!Array.isArray(search_list)) search_list = [search_list];
+
+    return new Promise((resolve, reject) => {
+      this.getAllTickets().then((all_tickets) => {
+        let unfound_tickets = search_list;
+        let resulting_ids = [];
+
+        for (let i = 0; i < all_tickets.length; i++) {
+          let idx = unfound_tickets.findIndex((el) => { return (el === all_tickets[i]['uuid']) })
+          if (idx >= 0) {
+            resulting_ids.push(i);
+            unfound_tickets.splice(idx, 1);
+          }
+        }
+
+        resolve({ 'found': resulting_ids, 'not_found': unfound_tickets });
+      }).catch((error) => reject(error));
+    });
+  }
 }
 
-function testTicketSearch() {
-  findTickets(['34634334290', '37675968758', '38857817400']).then((result) => {
-    console.log(result);
-  })
-}
+const ultimaker_tickets = new TicketList('Ultimakers');
